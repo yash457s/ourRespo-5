912a9c5005614060b95d39eb912bf378aef2887f
yashshivhare911119@gmail.com <yashshivhare911119@gmail.com>
2021-06-10 13:01:11 +0530

    Second commit

---
+++ Models/Emps.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{[Table("Emps")]
+    public class Emps
+    {
+        [Key]
+        public int EId { get; set; }
+        public string EName { get; set; }
+        public string EDesignation { get; set; }
+        public DateTime EDoj { get; set; }
+    }
+}
